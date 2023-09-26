a989a1356e56ff568d96587b580130762d19fad6
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-09-27 07:32:00 +0800

    feat: add b test input and output

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import subprocess
-import time
-numprobs = {'A': 2, 'B': 3, 'C': 3}
+numprobs = {'A': 2, 'B': 3, 'C': 5}
 
 app = Flask(__name__)
 
@@ -12,7 +11,6 @@ def index():
 @app.route('/process_form', methods=['POST'])
 def process_form():
     prob = request.form.get('choice')
-    # return f'{prob}'
     subprocess.run(["g++.exe", "-std=c++17", "sample.cpp", "-o", "sample"], encoding="utf-8")
     correct = 0
     for i in range(numprobs[prob]):
@@ -31,9 +29,6 @@ def process_form():
                     break
                 outputs.append(line.strip())
         test_out = subprocess.run(['sample'], capture_output=True, input=' '.join(inputs), encoding="utf-8")
-        print('\n'.join(outputs)+'\n')
-        print(' '.join(inputs))
-        print(test_out.stdout)
         correct += (test_out.stdout == '\n'.join(outputs)+'\n')
 
     return f'Correct Answers: {correct}/{numprobs[prob]}'
--- sample.cpp
+++ sample.cpp
@@ -3,14 +3,16 @@ typedef long long int ll;
 using namespace std;
  
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    ll n, m;
-    cin >> n >> m;
-    if(n == m<<1 || m == n<<1 || (max(n, m) - min(n,m) < min(n,m) && (n+m)%3 == 0))
-        cout << "YES\n";
-    else
-        cout << "NO\n";
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t;
+	cin >> t;
+	for(ll i=1; i<=t; i++){
+		ll ns = i*i, n2 = (ns*(ns-1))/2;
+		if(i > 2)
+			n2 -= 4*(i-1)*(i-2);
+		cout << n2 << "\n";
+	}
+	return 0;
 }
