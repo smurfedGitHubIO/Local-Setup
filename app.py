4e1e297d3bf6ab4e7dbdc25d9198fd90357ea812
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-09-26 10:27:58 +0800

    feat: add minimal working flask app

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import subprocess
+import time
+numprobs = {'A': 2, 'B': 3, 'C': 5}
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    prob = request.form.get('choice')
+    sample_out = subprocess.run(["g++.exe", "-std=c++17", "sample.cpp", "-o", "sample"], capture_output=True, encoding="utf-8")
+    correct = 0
+    for i in range(numprobs[prob]):
+        inputs = []
+        outputs = []
+        with open(f'./tests/input/{prob}/input_{i+1}.txt') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                inputs.append(line.strip())
+        with open(f'./tests/output/{prob}/output_{i+1}.txt') as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                outputs.append(line.strip())
+        test_out = subprocess.run(['sample'], capture_output=True, input=' '.join(inputs), encoding="utf-8")
+
+        correct += (test_out.stdout == '\n'.join(outputs)+'\n')
+
+    return f'Correct Answers: {correct}/{numprobs[prob]}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
